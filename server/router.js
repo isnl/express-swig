54660b595dcc06082eeda0d6d7edc92a244c022c
candy <htmlcs@163.com>
2020-02-27 15:15:49 +0800

    swig遍历

--- server/router.js
+++ server/router.js
@@ -1,36 +1,44 @@
-const express = require('express')
+const express = require("express");
 const appRouter = express.Router();
-appRouter.get('/', function (request, response) {
+appRouter.get("/", function(request, response) {
   try {
-    response.render('index', {
-      title: '首页',
-      content: 'Hello World'
-    })
+    response.render("index", {
+      title: "首页啊哦",
+      content: "Hello World",
+      list: [
+        { name: "张欢", age: 18 },
+        { name: "李露露", age: 18 }
+      ]
+    });
   } catch (error) {
-    console.log('404');
+    console.log("404");
   }
-})
-appRouter.get('/:id', function (request, response) {
+});
+appRouter.get("/:id", function(request, response) {
   try {
     response.render(request.params.id, {
-      title: '首页',
-      content: 'Hello World'
-    })
+      title: "首页",
+      content: "Hello World",
+      list: [
+        { name: "张欢", age: 18 },
+        { name: "李露露", age: 18 }
+      ]
+    });
   } catch (error) {
-    console.log('404');
+    console.log("404");
   }
-})
+});
 // 首页
-appRouter.get('/index', function (request, response) {
-  response.render('index.html', {
-    content: '首页'
-  })
-})
+appRouter.get("/index", function(request, response) {
+  response.render("index.html", {
+    content: "首页"
+  });
+});
 // demo1页面
-appRouter.get('/demo1', function (request, response) {
-  response.render('page/demo1.html', {
-    content: 'demo1页面'
-  })
-})
+appRouter.get("/demo1", function(request, response) {
+  response.render("page/demo1.html", {
+    content: "demo1页面"
+  });
+});
 
-module.exports = appRouter
+module.exports = appRouter;
